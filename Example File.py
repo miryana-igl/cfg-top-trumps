3c578d1e768ed45ff9a638f34c05f3b040e44e3e
J-Whitehead95 <72883275+J-Whitehead95@users.noreply.github.com>
2020-10-18 16:08:03 +0100

    j's example file

--- Example File.py
+++ Example File.py
@@ -1,7 +1,21 @@
 import random
 import requests
-def random_pokemon ():
-    pokemon_number = random.randint(1, 151)
+
+def random_pokemon():
+    def random_pokemon ():
+        pokemon_number = random.randint(1, 151)
+    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format("pokemon_number")
+    response = requests.get(url)
+    pokemon = response.json()
+    return{
+    'name': pokemon['name'],
+    'id': pokemon['id'],
+    'height': pokemon['height'],
+    'weight': pokemon['weight'],
+}
+
+def run():
+
     url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
     response = requests.get(url)
     pokemon = response.json()
@@ -13,6 +27,20 @@ def random_pokemon ():
     }
 def run ():
     my_pokemon = random_pokemon()
+print('You were given {}'.format(my_pokemon['name']))
+stat_choice = input('Which stat do you want to use? (id, height, weight) ')
+opponent_pokemon = random_pokemon()
+print('The opponent chose {}'.format(opponent_pokemon['name']))
+my_stat = my_pokemon[stat_choice]
+opponent_stat = opponent_pokemon[stat_choice]
+if my_stat > opponent_stat:
+    print('You Win!')
+elif my_stat < opponent_stat:
+    print('You Lose!')
+else:
+    print('Draw!')
+run ()
+
     print('You were given {}'.format(my_pokemon['name']))
     stat_choice = input('Which stat do you want to use? (id, height, weight) ')
     opponent_pokemon = random_pokemon()
@@ -26,3 +54,4 @@ def run ():
     else:
         print('Draw!')
 run()
+
