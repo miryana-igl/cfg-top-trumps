c57dddb1bf1157a62ac8863343ed83a073578fb9
Miry Ivanova <miryana@electronictheatre.com>
2020-10-19 13:06:04 +0100

    Added the Working Code
    Working code is example code + dice and stats

--- Working Code.py
+++ Working Code.py
@@ -1,21 +1,21 @@
+
 import random
 import requests
 
-#This bit of the code rolls a die and tells you if you go first,
-# ideally if you lose, the opponent bit should run (opponent_turn) so far broken
-
+#This bit of the code rolls a die and tells you if you go first
 
 def roll_die():
     player = input('Player name: ')
     your_roll = input('Type roll to roll a die ')
-
+    win= print('{} goes first.'.format(player))
+    lose= print('{} looses. Opponent goes first.'.format(player))
 
     your_choice = random.randint(1, 6)
     opponent = random.randint(1,6)
     if your_choice > opponent:
-       print('You go first.')
+       win
     else:
-        print('{} looses. Opponent goes first.'.format(player))
+        lose
 roll_die()
 
 #This bit of the code gets a random number and assigns the Pokemon to the player and the opponent
@@ -32,25 +32,16 @@ def random_pokemon ():
         'weight': pokemon['weight'],
     }
 
-def random_stat():
-    stat_choice = random.randint(1,3)
-    random_pokemon()
-def my_turn ():
+def run ():
     my_pokemon = random_pokemon()
     print('You were given {}'.format(my_pokemon['name']))
     print('The {n} stats are: \nID number: {i} \nHeight: {h} \nWeight: {w}'.format(n = my_pokemon['name'], i = my_pokemon['id'], h = my_pokemon['height'], w = my_pokemon['weight']))
 
     stat_choice = input('Which stat do you want to use? (id, height, weight) ')
-    my_stat = my_pokemon[stat_choice]
-my_turn()
-
-#I've split the 2 turns into functions thinking maybe we can call them in the dice loop, so far no idea how to do that
-
-def opponent_turn ():
     opponent_pokemon = random_pokemon()
-    print('The opponent was given {}'.format(opponent_pokemon['name']))
+    print('The opponent chose {}'.format(opponent_pokemon['name']))
     print('{n}''s stats are: \nID number: {i} \nHeight: {h} \nWeight: {w}'.format(n = opponent_pokemon['name'], i = opponent_pokemon['id'], h = opponent_pokemon['height'], w = opponent_pokemon['weight']))
-    stat_choice = random_stat
+    my_stat = my_pokemon[stat_choice]
     opponent_stat = opponent_pokemon[stat_choice]
     if my_stat > opponent_stat:
         print('You Win!')
@@ -58,4 +49,4 @@ def opponent_turn ():
         print('You Lose!')
     else:
         print('Draw!')
-opponent_turn()
+run()
