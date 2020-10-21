dafbc42aa583281a3af63826e5ce4dbd47b65fe1
Miry Ivanova <miryana@electronictheatre.com>
2020-10-21 18:19:05 +0100

    committing becasue can't pull

--- top_trump.py
+++ top_trump.py
@@ -1,15 +1,5 @@
-import requests
-import random
-
-def random_number():
-    pokemon_number = random.randint(1,151)
-    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
-    response = requests.get(url)
-    pokemon = response.json()
-    return {
-        'name': pokemon['name'],
-        'id': pokemon['id'],
-        'height': pokemon['height'],
-        'weight': pokemon['weight'],
-    }
-print(random_number())
+import turtle
+win = turtle.Screen()
+win.bgpic('giphy.gif')
+turtle.speed ('fastest')
+turtle.done()
